96a096eb60bacb730d8b32ccf177578ce485dd2c
Helluys <luys@live.fr>
2016-11-27 11:55:22 +0100

    LiveARApp improved
    Put the error checking inside, added tracking

--- src/LiveARApp.cpp
+++ src/LiveARApp.cpp
@@ -1,25 +1,46 @@
 #include "LiveARApp.h"
 
-LiveARApp::LiveARApp(unsigned width, unsigned height, int argc, char **argv, unsigned camera) : mApp(argc, argv), mViewer(width, height), mCameraID(camera)
+LiveARApp::LiveARApp(unsigned width, unsigned height, int &argc, char **argv, unsigned camera) : mApp(argc, argv), mViewer(width, height), mTracker(mViewer), mCameraID(camera)
 {
+    mReferenceImage = new PoLAR::Image_uc("reference.png", true);
+    mTracker.setReferenceImage(*mReferenceImage.get());
 }
 
 LiveARApp::~LiveARApp()
-{}
+{
 
-int LiveARApp::go()
+}
+
+int LiveARApp::exec()
 {
     mCamera = new PoLAR::VideoPlayer(mCameraID);
-    if(!mCamera.get())
-        throw LiveARException("Invalid camera ID", LiveARException::INVALID_CAMERA);
+    while(!mCamera->valid())
+    {
+        std::cerr << "Invalid camera, enter another camera ID (integer, -1 to quit) : ";
+        std::cin >> mCameraID;
+        if(mCameraID == -1) return -1;
+        mCamera = new PoLAR::VideoPlayer(mCameraID);
+    }
 
     mImage = new PoLAR::Image_uc(mCamera.get());
     mViewer.setBgImage(mImage.get());
 
     mCamera->play();
     mViewer.center();
-    mViewer.show();
 
+    mObject3D = new PoLAR::FrameAxis;
+    osg::Group *scene = mViewer.getScene3D();
+    scene->addChild(mObject3D.get());
+
+    mViewer.show();
     mApp.connect(&mApp, SIGNAL(lastWindowClosed()), &mApp, SLOT(quit()));
+    QObject::connect(mCamera, SIGNAL(newFrame(unsigned char*,int,int,int)),
+                        &mTracker, SLOT(newFrameReceived(unsigned char*,int,int,int)));
+
     return mApp.exec();
+}
+
+void LiveARApp::loadObject(const std::string &filename)
+{
+    mObject3D = new PoLAR::FrameAxis;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,5 @@ int main(int argc, char **argv)
 {
     LiveARApp app(800, 600, argc, argv);
 
-    bool run = false;
-    int appReturn = 0;
-    while(!run)
-    {
-        try
-        {
-            appReturn = app.go();
-            run = true;
-        }
-        catch(LiveARException &e)
-        {
-            std::cerr << e.what() << std::endl;
-            switch(e.type)
-            {
-                case LiveARException::UNKNOWN:
-                    return -1;
-                case LiveARException::INVALID_CAMERA:
-                    app.setCamera(1);
-                    break;
-            }
-        }
-    }
-
-    return appReturn;
+    return app.exec();
 }
